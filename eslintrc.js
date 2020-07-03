9fbb8c8127a52f522b7d44abb2dac393dc37224f
ushironoko <apple19940820@gmail.com>
2020-07-04 02:52:41 +0900

    update

--- eslintrc.js
+++ eslintrc.js
@@ -1,27 +1,25 @@
 module.exports = {
-  'env': {
-    'browser': true,
-    'es2020': true,
+  env: {
+    browser: true,
+    es2020: true,
   },
-  'extends': [
-    'plugin:@typescript-eslint/eslint-recommended',
+  extends: [
+    'eslint:recommended',
     'plugin:@typescript-eslint/recommended',
-    'prettier/vue',
     'plugin:vue/vue3-recommended',
+    'plugin:prettier/recommended',
+    'prettier/@typescript-eslint',
+    'prettier/vue',
   ],
-  'parser': 'vue-eslint-parser',
-  'parserOptions': {
-    'parser': '@typescript-eslint/parser',
-    'ecmaVersion': 2020,
-    'warnOnUnsupportedTypeScriptVersion': false,
-    'extraFileExtensions': ['.vue']
+  parserOptions: {
+    parser: '@typescript-eslint/parser',
+    ecmaVersion: 2020,
+    warnOnUnsupportedTypeScriptVersion: false,
+    extraFileExtensions: ['.vue'],
   },
-  'plugins': [
-    'prettier',
-    '@typescript-eslint'
-  ],
-  'rules': {
-    'quotes': ['error', 'single'],
+  plugins: ['@typescript-eslint'],
+  rules: {
+    quotes: ['error', 'single'],
     '@typescript-eslint/explicit-function-return-type': 0,
     '@typescript-eslint/explicit-module-boundary-types': 0,
     'vue/component-name-in-template-casing': [
@@ -29,18 +27,16 @@ module.exports = {
       'PascalCase',
       {
         ignores: [
-          // vue core
           'keep-alive',
           'transition',
           'transition-group',
           'component',
           'slot',
-
-          // vue-router
           'router-link',
-          'router-view'
-        ]
-      }
+          'router-view',
+          'nuxt-link',
+        ],
+      },
     ],
   },
 }
